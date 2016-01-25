23ef8dd967e8dd43a11a40c3ddddc8be93f51607
Iksman <wesselmjr@gmail.com>
2016-01-25 11:02:34 +0100

    superFighters

---
+++ Zoomtest/GetRandomEvent.py
@@ -0,0 +1,11 @@
+from Node import *
+
+class sFighter:
+  def __init__(self,name,one,two,three,four,five,six):
+    self.Name = name
+    self.One = one
+    self.Two = two
+    self.Three = three
+    self.Four = four
+    self.Five = five
+    self.Six = six
