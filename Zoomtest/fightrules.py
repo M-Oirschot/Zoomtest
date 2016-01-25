7862b7537a9d842ce6bad3315c34837842d2ac01
0911707 <0911707@hr.nl>
2016-01-25 14:52:40 +0100

    psuedo code in place

--- Zoomtest/fightrules.py
+++ Zoomtest/fightrules.py
@@ -1,3 +1,22 @@
 import pygame
+import Dice
 
-print("hello world")
+def checkFight():
+    if player1.pos == player2.pos:
+        print("he")
+    elif player1.pos == player3.pos:
+        print("dfsd")
+    elif  player1.pos == player4.pos:
+        print("sdjfsdf")
+
+def fight(pOne, pTwo):
+    pOneDice = dice(6)
+    pTwoDice = dice(6)
+    #p1 choice of attack
+    #p2 choice of attack
+    #p1 hp minus p2 attack
+    #p2 cond - p2 cond point cost
+    #p2 hp minus p1 attack
+    #p1 cond - p1 cond point used  
+
+
