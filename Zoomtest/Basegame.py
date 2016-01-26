5f09ef4960a6560e3a7fa60208b41b602a2c1819
M-Oirschot <marcovanoirschot@outlook.com>
2016-01-26 12:20:27 +0100

    asd

--- Zoomtest/Basegame.py
+++ Zoomtest/Basegame.py
@@ -22,22 +22,22 @@ def Menushit(screen,width,height):
     done = False
     while not done:
         
-        screen.blit(helpBtn,(GetCenter(width, height, playerName)[0] - (width / 3.525), GetCenter(width,height, playerName)[1] - (height / 3.525)))
+        screen.blit(helpBtn,(GetCenter(width, height, helpBtn)[0] - (width / 3.525), GetCenter(width,height, helpBtn)[1] - (height / 3.525)))
         screen.blit(scBtn,(GetCenter(width, height, playerName)[0] - (width / 2.4), GetCenter(width,height, playerName)[1] - (height / 3.525)))
         screen.blit(rollDiceBtn,(GetCenter(width, height, playerName)[0] - (width / 2.4), GetCenter(width,height, playerName)[1] - (height / 3)))
            
         pygame.event.get()
 
-        if (pygame.mouse.get_pressed()==(1,0,0) and helpBtn.get_rect(topleft=(GetCenter(width,height,helpBtn)[0], GetCenter(width,height,helpBtn)[1] + (height / 3.525))).collidepoint(pygame.mouse.get_pos())):
+        if (pygame.mouse.get_pressed()==(1,0,0) and helpBtn.get_rect(topleft=(GetCenter(width,height,helpBtn)[0] - (width / 3.525), GetCenter(width,height,helpBtn)[1] - (height / 3.525))).collidepoint(pygame.mouse.get_pos())):
             print("Help")
+            done = True
 
         pygame.display.flip()
 
 def Main(screen,width,height):
-    bg = pygame.transform.scale(pygame.image.load("content\\bordspel_background.png"), (width,height))
+    bg = pygame.image.load("content\\bordspel_background.png")
     board = build_square_board(11,5)
 
-    
     while True:
         screen.fill(white)
         screen.blit(bg, (0, 0))
@@ -48,6 +48,9 @@ def Main(screen,width,height):
         screen.blit(diceImg, (GetCenter(width, height, playerName)[0] - (width / 2.4), GetCenter(width,height, playerName)[1] + (height / 2.825)))
 
         pygame.draw.rect(screen,blue,(200,150,100,50))
+
+
+
 
         print("")
         pygame.display.flip()
