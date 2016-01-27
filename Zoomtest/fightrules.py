6a32070916bd2274b3205b10fbcd1ef6f2d8b6d0
0911707 <0911707@hr.nl>
2016-01-27 11:41:41 +0100

    finished sfight

--- Zoomtest/fightrules.py
+++ Zoomtest/fightrules.py
@@ -1,11 +1,11 @@
 import pygame
 
 def superfight(player, fight):
-    #if player pos = superfight tile
     playerdmg = fight[0]
     playercond = fight[1]
     sdmg = fight[2]
 
     if sdmg > playerdmg:
         sdmg -= playerdmg
-     
+        player.Lifepoints -= sdmg
+        player.conditionpoints -= playercond
