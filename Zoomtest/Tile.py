b5923967f2a83986418df4c81b33200632602dff
0911707 <0911707@hr.nl>
2016-01-28 12:27:02 +0100

    jkjkjkjkkjkjk

--- Zoomtest/Tile.py
+++ Zoomtest/Tile.py
@@ -2,16 +2,14 @@
 from Node import *
 
 #Tile properties
-notTraversable = 0
 startingTile = 1
 fighterTile = 2
 normalTile = 3
 
 class Tile:
-    def __init__(self,position, coords, startingTile):
-        self.sTile = False
-        self.co = coords
+    def __init__(self,position, startingTile, properties):
         self.pos = position
+        self.properties = properties
 
 def build_square_board(dimension, offset):
    
@@ -62,9 +60,10 @@ def build_square_board(dimension, offset):
     colOffset = 600
     rowOffset = 200
 
+    '''
     for firstRow in range(dimension):
         tRow = firstRow * rowOffset + standardOffset
-        for lastColumn in range (dimension):
+        for lastColumn in range(dimension):
             tCol = lastColumn * colOffset
             if(firstRow == 0):
                 if(lastColumn == 0):
@@ -105,6 +104,68 @@ def build_square_board(dimension, offset):
                     node = Node(Tile((firstColumn, lastRow), (0, 0), False), node)
                 else:
                     node = Node(Tile((firstColumn, lastRow), (0, 0), True), node)
+    '''
+    for topRow in range(dimension):
+        if topRow == 0:
+            node = Node(Tile((x,y), 1),node)
+            x += 78
+        elif topRow == 4:
+            node = Node(Tile((x,y),3),node)
+            x += 117
+        elif topRow == 5:
+            node = Node(Tile((x,y),2),node)
+            x += 117
+        elif topRow == dimension:
+            node = Node(Tile((x,y),1),node)
+            y += 127
+        else:
+            node = Node(Tile((x,y),3),node)
+            x  += 78
+
+    for rightRow in range(10):
+        if rightRow == 3:
+            node = Node(Tile((x,y),3),node)
+            y += 117
+        elif rightRow == 4:
+            node = Node(Tile((x,y),2),node)
+            y += 117
+        elif rightRow == 10:
+            node = Node(Tile((x,y),1),node)
+        else:
+            node = Node(Tile((x,y),3),node)
+            y += 78
+
+    for bottomRow in range(10):
+        if bottomRow == 3:
+            node = Node(Tile((x,y),3),node)
+            x -= 117
+        elif bottomRow == 4:
+            node = Node(Tile((x,y),2),node)
+            x -= 117
+        elif bottomRow == 10:
+            node = Node(Tile((x,y),1),node)
+        else:
+            node = Node(Tile((x,y),3),node)
+            x -= 78
+
+    for leftRow in range(9):
+        if leftRow == 3:
+            node = Node(Tile((x,y),3),node)
+            y -= 117
+        elif leftRow == 4:
+            node = Node(Tile((x,y),2),node)
+            y -= 117
+        else:
+            node = Node(Tile((x,y),2),node)
+
+        
+        
+        
+
+
+
 
     return(node)
+
+
 
