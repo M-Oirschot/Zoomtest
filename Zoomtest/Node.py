866ecfd5e1436009772bc7b43a7a69b4922b87c7
M-Oirschot <marcovanoirschot@outlook.com>
2016-01-29 12:08:44 +0100

    indent fix

--- Zoomtest/Node.py
+++ Zoomtest/Node.py
@@ -1,11 +1,11 @@
-class Node:
-  def __init__(self, value, tail):
-    self.Tail = tail
-    self.Value = value
-    self.IsEmpty = False
+﻿class Node:
+    def __init__(self, value, tail):
+        self.Tail = tail
+        self.Value = value
+        self.IsEmpty = False
 
 class Empty: 
-  def __init__(self):
-    self.IsEmpty = True
+    def __init__(self):
+        self.IsEmpty = True
 
 Empty = Empty()
