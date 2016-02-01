5c1127d4331a6a03d9b60bbbd49fc4cce5fa92a5
0911707 <0911707@hr.nl>
2016-02-01 10:47:37 +0100

    fsdfasdfsf

--- Zoomtest/entername_here.py
+++ Zoomtest/entername_here.py
@@ -56,13 +56,14 @@ def name(list,screen,width,height):
             
             playerindic = font.render("Type player " + str(playercount) + " name:", True, (0,0,0))
             if playercount == 1:
-              screen.blit(pygame.image.load("content\\player1pion.png"), (GetCenter(width,height,pygame.image.load("content\\player1pion.png"))[0],GetCenter(width,height,pygame.image.load("content\\player1pion.png"))[1] + (height / 8)))
+                screen.blit(pygame.image.load("content\\player1pion.png"), (GetCenter(width,height,pygame.image.load("content\\player1pion.png"))[0],GetCenter(width,height,pygame.image.load("content\\player1pion.png"))[1] + (height / 8)))
             elif playercount == 2:
-              screen.blit(pygame.image.load("content\\player3pion.png"), (GetCenter(width,height,pygame.image.load("content\\player1pion.png"))[0],GetCenter(width,height,pygame.image.load("content\\player1pion.png"))[1] + (height / 8)))
+                screen.blit(pygame.image.load("content\\player3pion.png"), (GetCenter(width,height,pygame.image.load("content\\player1pion.png"))[0],GetCenter(width,height,pygame.image.load("content\\player1pion.png"))[1] + (height / 8)))
             elif playercount == 3: 
-              screen.blit(pygame.image.load("content\\player2pion.png"), (GetCenter(width,height,pygame.image.load("content\\player1pion.png"))[0],GetCenter(width,height,pygame.image.load("content\\player1pion.png"))[1] + (height / 8)))
+                screen.blit(pygame.image.load("content\\player2pion.png"), (GetCenter(width,height,pygame.image.load("content\\player1pion.png"))[0],GetCenter(width,height,pygame.image.load("content\\player1pion.png"))[1] + (height / 8)))
             else:
-              screen.blit(pygame.image.load("content\\player4pion.png"), (GetCenter(width,height,pygame.image.load("content\\player1pion.png"))[0],GetCenter(width,height,pygame.image.load("content\\player1pion.png"))[1] + (height / 8)))
+                screen.blit(pygame.image.load("content\\player4pion.png"), (GetCenter(width,height,pygame.image.load("content\\player1pion.png"))[0],GetCenter(width,height,pygame.image.load("content\\player1pion.png"))[1] + (height / 8)))
+            
             block = otherfont.render(name, True, (0, 0, 0))
             rect = block.get_rect()
             rect.center = screen.get_rect().center
